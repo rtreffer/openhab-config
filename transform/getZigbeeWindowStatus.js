e5d55958ddac80df1d05052f5a07b41ea46c5cf1
Rene Treffer <rene.treffer@soundcloud.com>
2018-10-28 10:32:15 +0100

    Use window sensor as signal to turn the TV on/off

--- transform/getZigbeeWindowStatus.js
+++ transform/getZigbeeWindowStatus.js
@@ -1,19 +1,6 @@
 (function(x){
     var result = "";
     var json = JSON.parse(x);
-    // sanity check: is the device marked online?
-    if (json.hasOwnProperty('device')) {
-      var device = json.device;
-      if (device.hasOwnProperty('status') && device['status'] != 'online') {
-        return "";
-      }
-    }
-    // sanity check: link quality is > 0
-    if (json.hasOwnProperty('linkquality')) {
-      if (json.linkquality <= 0) {
-        return "";
-      }
-    }
     if (json.hasOwnProperty('contact')) {
       if (json.contact) {
         result="ON";
